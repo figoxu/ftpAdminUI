dba31af95cbfc64393d4992cdf0ff16f3cb37bec
Figo.Xu <xujh945@qq.com>
2016-12-16 15:45:49 +0800

    Add Login UI

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -3,7 +3,7 @@ import {Component } from 'angular2/core';
 @Component({
     selector:'login',
     templateUrl:'app/components/login/login.html',
-  	styleUrls: ['app/components/login/login.css'],
+  	styleUrls: [],
     providers: [],
     directives: [],
     pipes: []
