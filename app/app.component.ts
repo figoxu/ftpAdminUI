fd226abf131951275dd314fe4cc6fe1f4003a3b9
Figo.Xu <xujh945@qq.com>
2016-12-17 16:36:39 +0800

    Remove Sample Code

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,34 +1,9 @@
 import {Component} from "@angular/core";
 @Component({
     selector:'app',
-    styles:[`
-a{
-    text-decoration: none;
-}
-
-a.active{
-    font-weight: bold;
-}
-`],
     template: `
-<nav>
-    <a *ngFor="let nav of navs" 
-        [routerLink]="nav.url" 
-        routerLinkActive="active"
-        [routerLinkActiveOptions]="{exact:true}"
-        >
-        {{nav.content}}
-    </a>
-</nav>
 <router-outlet></router-outlet>
 `
 })
 export class AppComponent{
-    navs = [
-        {url: "", content: "Home"},
-        {url: "contacts", content: "Contacts"},
-        {url: "login", content: "Login"},
-        {url: "admin", content: "Admin"},
-        {url: "contacts/1", content: "One"},
-    ];
 }
--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,9 +1,7 @@
 import {RouterModule} from "@angular/router";
 
 const routes = [
-    {path: '', loadChildren: 'app/home/home.module'},
-    {path: 'contacts', loadChildren: 'app/contacts/contacts.module'},
-    {path: 'login', loadChildren: 'app/login/login.module'},
+    {path: '', loadChildren: 'app/login/login.module'},
     {path: 'admin', loadChildren: 'app/admin/admin.module'}
 ];
 
