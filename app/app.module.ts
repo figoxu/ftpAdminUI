5509261395bf8f07eb4fbd9474b8f0605a116c40
figoxu <xujh945@gmail.com>
2017-01-02 11:20:33 +0800

    Give Up Angular2 Version

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,19 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
 import {NgModule} from "@angular/core";
+import { HttpModule, JsonpModule } from '@angular/http';
 import appRoutes from "./app.routes";
 import {APP_BASE_HREF} from "@angular/common";
+import { DialogModule } from '@progress/kendo-angular-dialog';
+import { GridModule } from '@progress/kendo-angular-grid';
 
 @NgModule({
-    imports:[BrowserModule, appRoutes],
+    imports:[BrowserModule,
+        HttpModule,JsonpModule,appRoutes
+        ,
+        GridModule,
+        DialogModule
+    ],
     declarations:[AppComponent],
     bootstrap:[AppComponent]
 })
