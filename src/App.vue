941967aa97f7a95ce4fca9a87fc45bc49e053a08
figoxu <xujh945@gmail.com>
2017-01-29 20:38:53 +0800

    Add Login Action

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
   <div id="app">
-    <login></login>
+    <login v-if="!loginFlag" @logined="dispatchLogin"></login>
+    <admin v-else></admin>
   </div>
 </template>
 
 <script>
 import Login from './cmp/Login'
+import Admin from './cmp/Admin'
 
 export default {
   name: 'app',
   components:{
-    "login":Login
+    "login":Login,
+    "admin":Admin
+  },
+  data () {
+    return {
+      loginFlag:false
+    }
+  },
+  methods:{
+    dispatchLogin(flag) {
+      console.log("==  dispatch ===>")
+      this.loginFlag=flag
+    }
+
   }
 }
 </script>
--- src/cmp/Login.vue
+++ src/cmp/Login.vue
@@ -12,9 +12,21 @@
         <div class="form-group">
           <input type="password" class="input-sm validate[required] form-control" placeholder="密码">
         </div>
-        <button class="btn btn-gr-gray btn-block btn-xs">登录</button>
+        <button @click="loginAction" class="btn btn-gr-gray btn-block btn-xs">登录</button>
       </div>
     </div>
   </div>
 </template>
+<script>
+  export default {
+    methods:{
+      loginAction() {
+        console.log("==> login")
+        this.$emit('logined',true);
+      }
+    }
+  }
+</script>
+
+
 
